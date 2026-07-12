490e5ff24ed4f54d7ad37e4ffbbbdba13c450cf0
andreparames <andreparames@users.noreply.github.com>
2026-07-12 13:55:43 +0100

    fix: update Android build toolchain for emulator build (#57)
    * fix: update Android build toolchain for emulator build
    
    * fix: use typed AppExtension/LibraryExtension with pluginManager.hasPlugin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.GradleException
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 
 plugins {
@@ -10,16 +11,13 @@ plugins {
 
 android {
     namespace = "com.dogsafield.dogsafield"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
@@ -27,7 +25,7 @@ android {
         applicationId = "com.dogsafield.dogsafield"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -53,6 +51,16 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,18 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    afterEvaluate {
+        if (pluginManager.hasPlugin("com.android.library")) {
+            configure<com.android.build.gradle.LibraryExtension> {
+                compileSdkVersion(36)
+            }
+        } else if (pluginManager.hasPlugin("com.android.application")) {
+            configure<com.android.build.gradle.AppExtension> {
+                compileSdkVersion(36)
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
